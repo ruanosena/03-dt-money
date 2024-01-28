70b8be35c816127dc486534a287b6eafd1775e55
ruanosena <ruosena@hotmail.com>
2024-01-28 13:37:52 -0300

    performance

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -4,8 +4,8 @@ import { ArrowCircleDown, ArrowCircleUp, X } from "@phosphor-icons/react";
 import * as zod from "zod";
 import { Controller, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useContext } from "react";
 import { TransactionsContext } from "../../contexts/TransactionsContext";
+import { useContextSelector } from "use-context-selector";
 
 const newTransactionFormSchema = zod.object({
 	description: zod.string(),
@@ -17,7 +17,9 @@ const newTransactionFormSchema = zod.object({
 type NewTransactionFormInputs = zod.infer<typeof newTransactionFormSchema>;
 
 export function NewTransactionModal() {
-	const { createTransaction } = useContext(TransactionsContext);
+	const createTransaction = useContextSelector(TransactionsContext, (context) => {
+		return context.createTransaction;
+	});
 
 	const {
 		control,
--- src/pages/Transactions/SearchForm/index.tsx
+++ src/pages/Transactions/SearchForm/index.tsx
@@ -3,8 +3,9 @@ import { SearchFormContainer } from "./styles";
 import { useForm } from "react-hook-form";
 import * as zod from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useContext } from "react";
 import { TransactionsContext } from "../../../contexts/TransactionsContext";
+import { useContextSelector } from "use-context-selector";
+import { memo } from "react";
 
 const searchFormSchema = zod.object({
 	query: zod.string(),
@@ -12,8 +13,11 @@ const searchFormSchema = zod.object({
 
 type SearchFormInputs = zod.infer<typeof searchFormSchema>;
 
-export function SearchForm() {
-	const { fetchTransactions } = useContext(TransactionsContext);
+function SearchFormComponent() {
+	const fetchTransactions = useContextSelector(
+		TransactionsContext,
+		(context) => context.fetchTransactions
+	);
 
 	const {
 		register,
@@ -37,3 +41,24 @@ export function SearchForm() {
 		</SearchFormContainer>
 	);
 }
+
+/* Por quê que um componente renderiza?
+	Hooks changed (mudou estado, contexto, reducer)
+	Props changed (mudou propriedades)
+	Parent rerendered (componente pai renderizou)
+*/
+
+/* Qual o fluxo de renderização? 
+	1. O React recria o HTML da interface daquele componente
+	2. Compara a versão do HTML recriada com a versão anterior
+	3. Se mudou alguma coisa, ele reescreve o HTML na tela
+*/
+
+/* Memo
+	0. Hooks changed, Props changed (deep comparison);
+	0.1 Comparar versão anterior dos hooks e props;
+	0.2 Se mudou, permite a nova renderização;
+	(pode ser menos performático quando não há muita estrutura para renderizar)
+*/
+
+export const SearchForm = memo(SearchFormComponent);
